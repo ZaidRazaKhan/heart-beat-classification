f209f5071c59ca92cdf47065826017c4fb7f6021
zaidrazakhan <zaid.kv5@gmail.com>
2019-10-29 23:38:44 -0400

    Added knn code for heart beat classification

---
+++ knn.py
@@ -0,0 +1,30 @@
+import numpy as np
+import sklearn
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+# Load data from numpy file
+X =  np.load('feat.npy')
+y =  np.load('label.npy').ravel()
+
+# Split data into training and test subsets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=0)
+
+# # Simple SVM
+# print('fitting...')
+# clf = SVC(C=20.0, gamma=0.00001)
+# clf.fit(X_train, y_train)
+# acc = clf.score(X_test, y_test)
+# print("acc=%0.3f" % acc)
+from sklearn.neighbors import KNeighborsClassifier 
+knn = KNeighborsClassifier(n_neighbors = 4).fit(X_train, y_train) 
+  
+# accuracy on X_test 
+accuracy = knn.score(X_test, y_test) 
+print(accuracy)
+  
+# creating a confusion matrix 
+knn_predictions = knn.predict(X_test)  
+cm = confusion_matrix(y_test, knn_predictions) 
